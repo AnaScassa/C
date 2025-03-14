37728e8bd190f55fb635db2f8eb5f8a08a2cc45e
Ana Caroline <140931809+Anatskk@users.noreply.github.com>
2025-03-14 15:39:47 -0300

    Add files via upload

---
+++ laco.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    float numNotas, nota, notaAtual, media;
+    int repeticao = 1;
+    float numAluno = 0;
+
+    while(repeticao == 1){   
+    
+    media = 0; notaAtual = 0; numNotas = 0;
+    numAluno++;
+    printf("Aluno N%.0f", numAluno);    
+
+    printf("\nInsira quantas notas vai ter: ");
+    scanf("%f", &numNotas);
+    
+    for(int x = 0 ; x < numNotas ; x++){
+
+        printf("Insira a nota: ");
+        scanf("%f", &nota);
+        
+        notaAtual = notaAtual + nota;
+    }
+    
+    media = notaAtual / numNotas;
+    
+    printf("A média é %.2f", media);
+
+    if(media >= 6){
+        printf("\nAluno aprovado!");
+    }
+    else if(media < 6){
+        ("\nAluno reprovado!");
+    }
+
+    printf("\nDigite '1' para calcular uma nova nota ");
+    scanf("%d", &repeticao);
+    } 
+
+}
